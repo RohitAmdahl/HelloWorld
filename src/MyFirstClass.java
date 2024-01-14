2cd193d826c5001276b518919644722ec0c21f3e
rohitAmdahl <rohit_designer@outlook.com>
2024-01-14 01:53:19 +0100

    testing how some easy logical operaters

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -3,8 +3,21 @@ public class MyFirstClass { // public keyword is access modifier.
     // a collection of statements, java looks when running a program.
     public static void main(String[] args )// describing method with (String[] args )
     {
-        System.out.print("Hello Rohit welcome to thw Java world");
+        System.out.println("Hello Rohit welcome to thw Java world");
+
+        // if conditional statement.
+        int myVariable = 25;
+        int myAnotherVariable = 101;// Integer always will be the whole number.
+        if (myVariable != 20){ // logical operator
+            System.out.println("you have the value 25");
+        }
+        if ((myVariable != myAnotherVariable) && (myAnotherVariable >= 25)){ // logical operator
+            System.out.println("myAnotherVariable is more then 25 ");
+        }
+
+
     }
     // static is java keyword that needs an understanding of OOP concept.
     // you can also create your own method.
+
 }
