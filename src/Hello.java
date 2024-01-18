bf8ea4320b08218068da543a15fd263f49f82fe5
rohitAmdahl <rohit_designer@outlook.com>
2024-01-18 23:08:33 +0100

    trying to push to github

--- src/Hello.java
+++ src/Hello.java
@@ -22,16 +22,40 @@ public class Hello {
         }
 
         int newValue = 400;
-        if (newValue == 40) {
-            System.out.println("newValue is 50");
-        }
+        if (newValue == 40) System.out.println("newValue is 50");
 
         boolean isCar = false;
-        if (!isCar) {
+        if (isCar) { // some reason code is running  why? (isCar = true)
+            // (isCar == true) i need to do this.
+            // valid shortcut in java (isCar)
             System.out.println("this code with not run");
         }
-        else {
-            System.out.println("this code will say car is false");
+
+        String makeOfCar = "Volkswagen"; // varable that asign to "Volkswagen"
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true; // ternary operator
+        if (isDomestic){
+            System.out.println("this car is Domestic to our country");
         }
+
+        int ageOfClient = 20;
+        String ageText = (ageOfClient >= 18) ? "over Eighteen" : "still not Valid eight person is Kid";
+        System.out.println("our client is" + ageText);
+
+        double myDouble = 20.00;
+        double mySecond = 80.00;
+
+        double addNumbers = (myDouble + mySecond) * 100.00 ;
+        System.out.println(addNumbers);
+        double myRiemder = (addNumbers % 40.00);
+        System.out.println(myRiemder);
+
+        boolean myTrue = (myRiemder == 0.00) ? true : false;
+        System.out.println(myTrue);
+
+        if (myTrue){
+            System.out.println("Got some remainder");
+        }
+
+
     }
 }
--- src/NewWorld.java
+++ src/NewWorld.java
@@ -1,6 +1,15 @@
 public class NewWorld {
     public static void main (String [] args){
         System.out.println("check if this works ");
+
+        int x = 5;
+        int  y = 40;
+        if ((x == 5) && (y == 40)) {
+            System.out.println("it is 5");
+        } else {
+            System.out.print("yes its 40");
+        }
+
     }
 
 }
